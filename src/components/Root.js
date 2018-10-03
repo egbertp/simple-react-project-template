e477a568affed3c5b1008fe353e43161143237c2
Egbert Pot <Egbert.Pot@rabobank.nl>
2018-10-03 22:30:36 +0200

    Remove the react-router for now.
    First I want a working React-only setup

--- src/components/Root.js
+++ src/components/Root.js
@@ -1,19 +1,12 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { ConnectedRouter } from 'connected-react-router';
+// import { ConnectedRouter } from 'connected-react-router';
 import App from './App';
 
 export default class Root extends Component {
   render() {
-    const { history } = this.props;
     return (
-      <ConnectedRouter history={history}>
-        <App />
-      </ConnectedRouter>
+      <App />
     );
   }
 }
-
-Root.propTypes = {
-  history: PropTypes.object.isRequired
-};
